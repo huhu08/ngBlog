c012e04987204fe2ee41bcef17c4794f403630b4
Huda <hudaelzubair@hotmail.com>
2024-06-28 20:44:42 +0300

    j

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
-  {path :'contact',component: ContactComponent}
+  {path :'contact',component: ContactComponent},
+  {path:'',pathMatch:'full',redirectTo:'articles'},
+  {path:'**',redirectTo:'articles'},
 ];
 
 @NgModule({
