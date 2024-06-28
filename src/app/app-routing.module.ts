22f15297c11366294882618a8157f73ce2b7437c
Huda <hudaelzubair@hotmail.com>
2024-06-28 13:54:46 +0300

    setting up nav and footer bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
+  {path :'contact',component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
